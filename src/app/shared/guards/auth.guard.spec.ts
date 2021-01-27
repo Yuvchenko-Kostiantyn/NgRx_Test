d3f4061c58d4453c367583cdec28db08e674661b
Kostiantyn Yuvchenko <yuvchenko.k.a@gmail.com>
2021-01-27 17:18:39 +0200

    Added imports for unit testing

--- src/app/shared/guards/auth.guard.spec.ts
+++ src/app/shared/guards/auth.guard.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
+  let initialState = {
+    userData: {
+        id: null,
+        username: '',
+        password: ''
+    },
+    tasks: [],
+    users: [],
+}
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [provideMockStore({initialState})]
+    });
     guard = TestBed.inject(AuthGuard);
   });
 
--- src/app/shared/services/tasks.service.spec.ts
+++ src/app/shared/services/tasks.service.spec.ts
@@ -1,12 +1,24 @@
 import { TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { TasksService } from './tasks.service';
 
 describe('TasksService', () => {
   let service: TasksService;
+  let initialState = {
+    userData: {
+        id: null,
+        username: '',
+        password: ''
+    },
+    tasks: [],
+    users: [],
+}
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideMockStore({initialState})]
+    });
     service = TestBed.inject(TasksService);
   });
 
